cb23f7040306b8c9d9d48829ff28cf01f6f1de24
junyi.zeng <junyi.zeng@dmall.com>
2021-04-07 17:37:25 +0800

    添加logback日志

--- src/main/java/com/zjy/testdocker/controller/TestController.java
+++ src/main/java/com/zjy/testdocker/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.zjy.testdocker.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    Logger log = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/index")
     public Object index() {
+        log.info("现在访问服务的index请求了！");
         return "准备就绪！";
     }
 }
