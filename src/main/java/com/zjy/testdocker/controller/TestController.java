db6baa2f4289b816ac2b174f428889578239435d
junyi.zeng <junyi.zeng@dmall.com>
2021-04-23 16:55:57 +0800

    接口注释生成工具

--- src/main/java/com/zjy/testdocker/controller/TestController.java
+++ src/main/java/com/zjy/testdocker/controller/TestController.java
@@ -5,12 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 测试类
+ */
 @RestController
 public class TestController {
     Logger log = LoggerFactory.getLogger(TestController.class);
 
+    /**
+     * 首页
+     * @param a 测试参数
+     * @return
+     */
     @GetMapping("/index")
-    public Object index() {
+    public String index(Long a) {
         log.info("现在访问服务的index请求了！");
         return "准备就绪1！";
     }
